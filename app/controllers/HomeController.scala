9255a71f51b0a3d24da2fb342bb4bc005f618b29
atsuki1224 <akeve1224@icloud.com>
2020-08-20 22:46:53 +0900

    commit

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,24 +1,35 @@
 package controllers
 
+/**
 import javax.inject._
 import play.api._
 import play.api.mvc._
-
 import play.api.libs.ws._
 import scala.concurrent.{Future, Await}
 import javax.inject.Inject
 import scala.concurrent.duration.Duration
-
 import play.api.libs.ws.WSClient
-
 import scala.concurrent.{ExecutionContext, Future}
+**/
+
+import javax.inject.Inject
+
+import scala.concurrent.{Future, Await}
+import scala.concurrent.duration._
+import play.api.mvc._
+import play.api.libs.ws._
+import play.api.http.HttpEntity
+import akka.actor.ActorSystem
+import akka.stream.scaladsl._
+import akka.util.ByteString
+
 /**
  *
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
-@Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents, ws: WSClient) extends BaseController {
+  /*
   implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
     def index() = Action { implicit request: Request[AnyContent] =>
       val url = "https://api.github.com/users/atsuki1224"
@@ -31,4 +42,22 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents, w
       println(text)
       Ok(views.html.index(text))
     }
+    */
+   def index()  = Action { implicit request: Request[AnyContent] =>
+  implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
+     val url = "https://api.github.com/users/atsuki1224"
+     val request: WSRequest = ws.url(url)
+     val complexRequest: WSRequest =
+     request
+       .addHttpHeaders("Accept" -> "application/json / bio")
+       .addQueryStringParameters("search" -> "play")
+       val futureResponse: Future[WSResponse] = complexRequest.get()
+       val text: WSResponse = Await.result(futureResponse, Duration.Inf)
+       println("これがあたいだよおおおおおおおおおおおおお")
+       println(text)
+       Ok(views.html.index())
+   }
 }
+
+
+
