44f311864396460417623b4d2337d7c5067629f4
atsuki1224 <akeve1224@icloud.com>
2020-08-23 23:55:54 +0900

    commit

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,6 +1,5 @@
 package controllers
 
-/**
 import javax.inject._
 import play.api._
 import play.api.mvc._
@@ -10,53 +9,22 @@ import javax.inject.Inject
 import scala.concurrent.duration.Duration
 import play.api.libs.ws.WSClient
 import scala.concurrent.{ExecutionContext, Future}
-**/
-
-import javax.inject.Inject
-
-import scala.concurrent.{Future, Await}
-import scala.concurrent.duration._
-import play.api.mvc._
-import play.api.libs.ws._
-import play.api.http.HttpEntity
-import akka.actor.ActorSystem
-import akka.stream.scaladsl._
-import akka.util.ByteString
-
 /**
  *
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 class HomeController @Inject()(val controllerComponents: ControllerComponents, ws: WSClient) extends BaseController {
-  /*
   implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
     def index() = Action { implicit request: Request[AnyContent] =>
-      val url = "https://api.github.com/users/atsuki1224"
+      val url = "https://collectionapi.metmuseum.org/public/collection/v1/objects/1224"
       //val url = "https://zipcloud.ibsnet.co.jp/api/search?zipcode=7830060"
       val request = ws.url(url)
       val result: Future[String] = request.get().map{
-        response => (response.json \ "bio").as[String]
+        response => (response.json \ "primaryImage").as[String]
       }
       val text: String = Await.result(result, Duration.Inf)
       println(text)
       Ok(views.html.index(text))
     }
-    */
-   def index()  = Action { implicit request: Request[AnyContent] =>
-  implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
-     val url = "https://api.github.com/users/atsuki1224"
-     val request: WSRequest = ws.url(url)
-     val complexRequest: WSRequest =
-     request
-       .addHttpHeaders("Accept" -> "application/json / bio")
-       .addQueryStringParameters("search" -> "play")
-       val futureResponse: Future[WSResponse] = complexRequest.get()
-       val text: WSResponse = Await.result(futureResponse, Duration.Inf)
-       println(text)
-       Ok(views.html.index())
-   }
 }
-
-
-
